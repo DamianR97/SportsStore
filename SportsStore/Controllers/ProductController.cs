6cc8b23fe22d013edf23b6a875a0c0820df72303
Adam Sobkowicz <Sobkowicz_64786@cloud.wsb.wroclaw.pl>
2019-02-21 21:57:32 +0100

    fix

--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
-using System.Linq;
 using SportsStore.Models.ViewModels;
 namespace SportsStore.Controllers
 {
