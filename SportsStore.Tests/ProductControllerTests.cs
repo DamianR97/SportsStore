23f63172a5a53c6c0b1f9fd66114ddb34dcccca7
DamianRodak <damian.rodak1@o2.pl>
2019-02-22 14:27:39 +0100

    Filtrowanie listy produktów

--- SportsStore.Tests/ProductControllerTests.cs
+++ SportsStore.Tests/ProductControllerTests.cs
@@ -23,8 +23,7 @@ namespace SportsStore.Tests
                 new Product {ProductID = 5, Name = "P5", Category = "Cat3"}
             }).AsQueryable<Product>());
             // Przygotowanie — utworzenie kontrolera i ustawienie 3-elementowej strony.
-            ProductController controller = new ProductController(mock.Object);
-            controller.PageSize = 3;
+            ProductController controller = new ProductController(mock.Object) {PageSize = 3};
             // Działanie.
             Product[] result =
                 (controller.List("Cat2", 1).ViewData.Model as ProductsListViewModel)
@@ -47,8 +46,7 @@ namespace SportsStore.Tests
                 new Product {ProductID = 4, Name = "P4"},
                 new Product {ProductID = 5, Name = "P5"}
             }).AsQueryable<Product>());
-            ProductController controller = new ProductController(mock.Object);
-            controller.PageSize = 3;
+            ProductController controller = new ProductController(mock.Object) {PageSize = 3};
             // Działanie.
             ProductsListViewModel result =
                 controller.List(null, 2).ViewData.Model as ProductsListViewModel;
