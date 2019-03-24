a57f0293036c3685cfd78f4adc3f2b35deba2031
Adam Sobkowicz <Sobkowicz_64786@cloud.wsb.wroclaw.pl>
2019-03-24 16:29:18 +0100

    Dodanie kontroli poprawności modelu

--- SportsStore/Models/Product.cs
+++ SportsStore/Models/Product.cs
@@ -1,11 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
 namespace SportsStore.Models
 {
     public class Product
     {
         public int ProductId { get; set; }
+
+        [Required(ErrorMessage = "Proszê podaæ nazwê produktu.")]
+        [Display(Name = "Nazwa")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Proszê podaæ opis.")]
+        [Display(Name = "Opis")]
         public string Description { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Proszê podaæ dodatni¹ cenê.")]
+        [Display(Name = "Cena")]
         public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Proszê okreœliæ kategoriê.")]
+        [Display(Name = "Kategoria")]
         public string Category { get; set; }
     }
 }
