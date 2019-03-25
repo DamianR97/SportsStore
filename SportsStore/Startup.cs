276b770493fc722a9e208e6601f93193bc76b699
Adam Sobkowicz <Sobkowicz_64786@cloud.wsb.wroclaw.pl>
2019-03-25 18:46:45 +0100

    Konfiguracja aplikacji

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SportsStore.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace SportsStore
 {
@@ -24,8 +25,17 @@ namespace SportsStore
         public void ConfigureServices(IServiceCollection services)
            {
             services.AddDbContext<ApplicationDbContext>(options =>
-                   options.UseSqlServer(
-                       Configuration["Data:SportStoreProducts:ConnectionString"]));
+                options.UseSqlServer(
+                    Configuration["Data:SportStoreProducts:ConnectionString"]));
+
+            services.AddDbContext<AppIdentityDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration["Data:SportStoreIdentity:ConnectionString"]));
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppIdentityDbContext>()
+                .AddDefaultTokenProviders();
+
             services.AddTransient<IProductRepository, EFProductRepository>();
             services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
@@ -41,6 +51,7 @@ namespace SportsStore
                app.UseStatusCodePages();
                app.UseStaticFiles();
                app.UseSession();
+               app.UseAuthentication();
                app.UseMvc(routes => {
                    routes.MapRoute(
                         name: null,
